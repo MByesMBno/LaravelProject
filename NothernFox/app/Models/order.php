70c862fd25fdd01823da83e910a7e30ca4cb3ee7
MbYesMbNo <akamegakill228@mail.ru>
2025-02-22 02:14:32 +0500

    'Models'

--- NothernFox/app/Models/order.php
+++ NothernFox/app/Models/order.php
@@ -3,20 +3,13 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
 class order extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'user_id',
-        'total_amount',
-        'status',
-    ];
-
-    public function items()
-    {
-        return $this->hasMany(order_item::class);
-    }
+  public function items(): BelongsToMany{
+    return $this->belongsToMany(Item::class, 'order_items')
+    ->withPivot(['price','quantity']);
+}
 }
--- NothernFox/routes/web.php
+++ NothernFox/routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
-use App\Http\Controllers\ShopController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('Hello', ['temp'=>'Hello World']);
-});
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/{id}', [CategoryController::class,'show']);
+Route::get('/item', [ItemController::class,'index']);
 
+Route::get('/order/{id}', [OrderController::class,'show']);
